964291f0b6840441b2b5a65c46000723d98a68fa
chocolate1290 <kmisuta425@gmail.com>
2020-11-17 15:58:10 +0900

    画面改修（ボタン追加）

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,13 @@
 <template>
   <div class="hello">
+    <button @click="updatetabemono('tainootukuri')">update</button><br/>
+    <!-- {{kaisenList}} -->
+    {{kaisenList2}}
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    {{myVar}}
+    {{kaisenn}}
+    <br/>
+    <button @click="updateText('Hello World!!')">update</button>
   </div>
 </template>
 
@@ -35,6 +16,36 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data: function(){
+    return {
+      myVar: "hogehoge",
+      kaisenn:[
+       { menyu: ["kaisenndonn", "osasimi", "agemono"] }
+      ]
+    }
+  },
+  methods:{
+    updateText: function(text){
+      this.myVar = text;
+      let myvar = "jpgajpga";
+      console.log(myvar)
+    },
+    updatetabemono: function(text){
+      this.kaisenn[0].menyu[3] = text;
+      this.kaisenn[0].menyu.push(text);
+      console.log(this.kaisenn)
+    }
+  },
+  computed: {
+    kaisenList: function(){
+      return this.kaisenn[0].menyu.join(" / ")
+    },
+    kaisenList2: function(){
+      alert("海鮮が更新されました。")
+      console.log(this.myVar)
+      return this.kaisenn[0].menyu
+    }
   }
 }
 </script>
@@ -43,16 +54,5 @@ export default {
 <style scoped>
 h3 {
   margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
 }
 </style>
